29dce0fcb793ee06d1be76c4694ebe4602c404ca
Jhoann <jhoannegd@gmail.com>
2023-04-20 22:19:26 +0000

    Ajustes finales app/index

--- src/app.js
+++ src/app.js
@@ -7,5 +7,38 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+
+  const palos_html = document.getElementById("palos");
+  const paletes = document.getElementById("palos_inferiores");
+  const valor_html = document.getElementById("rank");
+
+  const palos_color = ["♥", "♦", "♣", "♠"];
+
+  const valores = [
+    "As",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "Jota",
+    "Reina",
+    "Rey"
+  ];
+
+  const palos_aleaotrios =
+    palos_color[Math.floor(Math.random() * palos_color.length)];
+
+  const valores_aleatorios =
+    valores[Math.floor(Math.random() * valores.length)];
+
+  palos_html.innerHTML = `${palos_aleaotrios}`;
+
+  paletes.innerHTML = `${palos_aleaotrios}`;
+
+  valor_html.innerHTML = `${valores_aleatorios}`;
 };
